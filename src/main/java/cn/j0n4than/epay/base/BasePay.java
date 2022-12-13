002a05e94111bf03b045c2414829a3c7b0880f8a
S0tephen <S0tephen@outlook.com>
2022-12-13 20:47:29 +0800

    perf: 使用自己的md5加密方式, 取消依赖commons-codec

--- src/main/java/cn/j0n4than/epay/base/BasePay.java
+++ src/main/java/cn/j0n4than/epay/base/BasePay.java
@@ -1,7 +1,5 @@
 package cn.j0n4than.epay.base;
 
-import org.apache.commons.codec.digest.DigestUtils;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -19,7 +17,6 @@ public abstract class BasePay {
      * 异步通知验证
      *
      * @param data 请求的数据
-     *
      * @return true 验证成功, false 验证失败
      */
     public abstract boolean signVerify(Map<String, String[]> data);
@@ -30,7 +27,6 @@ public abstract class BasePay {
      * @param map 需要排序的Map
      * @param <K> key
      * @param <V> value
-     *
      * @return 排序后的Map
      */
     protected <K extends Comparable<? super K>, V> Map<K, V> sort(Map<K, V> map) {
@@ -46,11 +42,33 @@ public abstract class BasePay {
      * MD5加密
      *
      * @param str 需要加密的字符串
-     *
      * @return 加密后的字符串
      */
-    protected String md5(String str) {
-        return DigestUtils.md5Hex(str);
+    public static String md5(String str) {
+        // 生成一个MD5加密计算摘要
+        java.security.MessageDigest md;
+        try {
+            md = java.security.MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            byte[] b = md.digest();
+            // 16是表示转换为16进制数
+            int i;
+            StringBuilder buf = new StringBuilder();
+            for (byte value : b) {
+                i = value;
+                if (i < 0) {
+                    i += 256;
+                }
+                if (i < 16) {
+                    buf.append("0");
+                }
+                buf.append(Integer.toHexString(i));
+            }
+            return buf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-
 }
